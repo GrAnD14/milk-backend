76354fc43d5265f4671373eef5c3a9e3cb257f76
YarikMix <dan-555@list.ru>
2024-03-03 16:17:46 +0300

    Init

---
+++ app/management/commands/add_users.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+from ...models import *
+
+
+def add_users():
+    CustomUser.objects.create_user("user", "user@user.com", "1234")
+    CustomUser.objects.create_superuser("root", "root@root.com", "1234")
+
+    for i in range(2, 3):
+        CustomUser.objects.create_user(f"user{i}", f"user{i}@user.com", "1234")
+        CustomUser.objects.create_superuser(f"root{i}", f"root{i}@root.com", "1234")
+
+    print("Пользователи созданы")
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        add_users()
+
